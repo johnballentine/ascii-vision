7bfacaf2212efd841e0b3dfb813daa9c30c402fa
John Ballentine <johnballentine10@gmail.com>
2023-07-28 01:13:57 +0000

    Made canny an option

--- main.py
+++ main.py
@@ -43,16 +43,10 @@ def image_to_ascii(image, width: int, height: int):
     
     return ascii_str
 
-def convert_to_ascii(image_bytes):
+def convert_to_ascii(image_bytes, canny=True):  # Set the default value of canny to True
     # Load image if BytesIO object is provided
     if isinstance(image_bytes, BytesIO):
         image = cv2.imdecode(np.frombuffer(image_bytes.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
-
-        # Get Canny edges
-        edges = get_canny(image)
-
-        # Convert to ASCII art
-        ascii_art = image_to_ascii(edges, 80, 30)
 
         # Get the folder path to save the images and ASCII art
         folder_path = os.getcwd()  # You can change this to the desired folder path
@@ -61,9 +55,19 @@ def convert_to_ascii(image_bytes):
         orig_filename = make_filename(folder_path, "image_out-orig-", "png")
         cv2.imwrite(orig_filename, image)
 
-        # Save the Canny edge detection output
-        canny_filename = make_filename(folder_path, "image_out-canny-", "png")
-        cv2.imwrite(canny_filename, edges)
+        if canny:
+            # Get Canny edges
+            edges = get_canny(image)
+
+            # Save the Canny edge detection output
+            canny_filename = make_filename(folder_path, "image_out-canny-", "png")
+            cv2.imwrite(canny_filename, edges)
+
+            # Use Canny edges for ASCII conversion
+            image = edges
+
+        # Convert to ASCII art
+        ascii_art = image_to_ascii(image, 80, 30)
 
         # Save the ASCII art as a text file
         ascii_filename = make_filename(folder_path, "image_out-ascii-", "txt")
@@ -71,13 +75,15 @@ def convert_to_ascii(image_bytes):
             file.write(ascii_art)
 
         print(f"Conversion and saving completed.")
-        return ascii_art
 
-def url_to_ascii(image_url):
+    return ascii_art
+
+
+def url_to_ascii(image_url, canny=True):  # Set the default value of canny to True
     # Get BytesIO object of the image from the URL
     image_bytes = url_to_image(image_url)
-    # Convert to ASCII art using convert_to_ascii
-    ascii_art = convert_to_ascii(image_bytes)
+    # Convert to ASCII art using convert_to_ascii with the canny argument
+    ascii_art = convert_to_ascii(image_bytes, canny=canny)  # Pass the canny argument down
     return ascii_art
 
 def url_to_image(image_url):
@@ -98,22 +104,30 @@ def save_bytesio_to_png(image_bytes, base_filename="image_out-"):
 
     print(f"Image saved as {filename}")
 
+import os
+
 def make_filename(folder_path, base_filename, extension):
+    # Create the 'output' directory if it doesn't exist
+    output_folder = os.path.join(folder_path, "output")
+    os.makedirs(output_folder, exist_ok=True)
+
     i = 1
     while True:
         filename = f"{base_filename}{str(i).zfill(5)}.{extension}"
-        filepath = os.path.join(folder_path, filename)
+        filepath = os.path.join(output_folder, filename)
         if not os.path.exists(filepath):
             return filepath
         i += 1
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert an image to ASCII art.")
     parser.add_argument("--url", type=str, help="URL of the image to convert to ASCII art")
+    parser.add_argument("--canny", action="store_true", help="Enable Canny edge detection")  # Changed the argument name
     args = parser.parse_args()
 
     if args.url:
-        ascii_art_url = url_to_ascii(args.url)
+        ascii_art_url = url_to_ascii(args.url, canny=args.canny)  # Pass the canny argument here
         print("Image URL to ASCII Art:")
         print(ascii_art_url)
     else:
